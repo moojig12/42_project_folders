9cf2eba42372de8923fa0373138ea98918cb901d
moojig12 <moojig12>
2024-03-20 12:31:22 +0100

    Updates to philo and minishell folders

--- circle_3/minishell/srcs/main.c
+++ circle_3/minishell/srcs/main.c
@@ -6,7 +6,16 @@
 /*   By: nmandakh <nmandakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 11:37:48 by nmandakh          #+#    #+#             */
-/*   Updated: 2024/03/20 11:37:48 by nmandakh         ###   ########.fr       */
+/*   Updated: 2024/03/20 12:15:49 by nmandakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+int	main(void)
+{
+	while(1)
+	{
+		printf("meow");
+	}
+}
--- circle_3/minishell/srcs/minishell.h
+++ circle_3/minishell/srcs/minishell.h
@@ -6,7 +6,16 @@
 /*   By: nmandakh <nmandakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 11:37:51 by nmandakh          #+#    #+#             */
-/*   Updated: 2024/03/20 11:37:51 by nmandakh         ###   ########.fr       */
+/*   Updated: 2024/03/20 12:16:51 by nmandakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINI_SHELL_H
+# define MINI_SHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+
+#endif
